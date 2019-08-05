234f0ca2f1fb41cde0959abf697ab33355e22bf2
Brandon Buchs <bbuchs@wisc.edu>
2019-08-05 12:59:03 -0400

    update main.js

--- labs/d3-lab/lib/main.js
+++ labs/d3-lab/lib/main.js
@@ -3,6 +3,7 @@ window.onLoad = setMap();
 
 function setMap() {
     d3.queue()
+        .defer(d3.json, 'data/trout.topojson')
         .defer(d3.csv, 'data/VAStockedTroutStreams.csv')
         .defer(d3.json, 'data/trout.topojson')
         .defer(d3.json, 'data/state.topojson')
