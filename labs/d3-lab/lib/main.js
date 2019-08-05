8c29b83fa73289005a1cb3eae68359656c4f938d
Brandon Buchs <bbuchs@wisc.edu>
2019-08-05 19:15:23 -0400

    updated scale

--- labs/d3-lab/lib/main.js
+++ labs/d3-lab/lib/main.js
@@ -15,7 +15,7 @@ function setMap() {
         .center([-79.0558, 35.9132])
         .rotate([0, 0, 0])
         .parallels([30, 40])
-        .scale(2500)
+        .scale(3500)
         .translate([width / 2, height / 2]);
 
     var path = d3.geoPath()
