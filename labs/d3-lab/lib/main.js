86ddc544831105210c98acec4f4cb0cb2138ac81
Brandon Buchs <bbuchs@wisc.edu>
2019-08-05 19:36:59 -0400

    update

--- labs/d3-lab/lib/main.js
+++ labs/d3-lab/lib/main.js
@@ -12,10 +12,10 @@ function setMap() {
         .attr('height', height);
 
     var projection = d3.geoConicEqualArea()
-        .center([-79.0558, 35.9132])
+        .center([-79.0558, 31.9132])
         .rotate([0, 0, 0])
         .parallels([30, 40])
-        .scale(5000)
+        .scale(4000)
         .translate([width / 2, height / 2]);
 
     var path = d3.geoPath()
