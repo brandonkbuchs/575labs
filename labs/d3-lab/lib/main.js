7f922c0fa7788fc051c437f923d437261a23756e
Brandon Buchs <bbuchs@wisc.edu>
2019-08-05 12:08:59 -0400

    Uploading New Lab

---
+++ labs/d3-lab/lib/main.js
@@ -0,0 +1,17 @@
+//begin script when window loads
+window.onLoad = setMap();
+
+function setMap() {
+    d3.queue()
+        .defer(d3.csv, 'data/VAStockedTroutStreams.csv')
+        .defer(d3.json, 'data/VAStockTroutStreams.json')
+        .defer(d3.json, 'data/VAState.json')
+        .await(callback);
+
+    function callback(error, csvData, streams, state) {
+        console.log(error);
+        console.log(csvData);
+        console.log(streams);
+        console.log(state);
+    };
+};
