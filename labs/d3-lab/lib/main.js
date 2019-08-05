1ec55a39ba52125bbc070d8a8d8fc552c3ac0159
Brandon Buchs <bbuchs@wisc.edu>
2019-08-05 13:51:17 -0400

    update main.js

--- labs/d3-lab/lib/main.js
+++ labs/d3-lab/lib/main.js
@@ -2,6 +2,21 @@
 window.onLoad = setMap();
 
 function setMap() {
+    var width = 960,
+        height = 460;
+    
+    var map = d3.select('body')
+        .append('svg')
+        .attr('class', 'map')
+        .attr('width', width)
+        .attr('height', height);
+
+    var projection = d3.geo.conicConformal()
+        .center([-79.0558, 35.9132])
+        .rotate([-2, 0, 0])
+        .parallels([43, 63])
+        .scale(2500)
+        .translate([width / 2, height / 2]);
     d3.queue()
         .defer(d3.csv, 'data/crashes.csv')
         .defer(d3.json, 'data/crashes.topojson')
@@ -14,8 +29,8 @@ function setMap() {
         console.log(crashes);
         console.log(city);
 
-        var chapelHill = topojson.feature(city, city.objects.chapelHill),
-            bikeCrashes = topojson.feature(crashes, crashes.objects.bikeCrashes).features;
+        var chapelHill = topojson.feature(city, city.objects.chapelhill),
+            bikeCrashes = topojson.feature(crashes, crashes.objects.crashes).features;
 
         console.log(chapelHill);
         console.log(bikeCrashes);
