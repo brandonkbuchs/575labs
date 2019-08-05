3c5d6e11b5b68ae4cc328167692afe62f071a31b
Brandon Buchs <bbuchs@wisc.edu>
2019-08-05 18:59:45 -0400

    updated main.js index.html style.css

--- labs/d3-lab/lib/main.js
+++ labs/d3-lab/lib/main.js
@@ -3,7 +3,7 @@ window.onLoad = setMap();
 
 function setMap() {
 
-    var width = 960, height = 460;
+    var width = window.innerWidth * 0.5, height = 460;
 
     var map = d3.select('body')
         .append('svg')
@@ -58,12 +58,98 @@ function setMap() {
 };//end of function setMap
 
 function setChart(csvData, colorScale){
-    var chartw = 550, charth = 460;
+    var chartw = window.innerWidth * 0.425, 
+        charth = 460,
+        leftp = 25,
+        rightp = 2,
+        topBottomp = 5,
+        chartInnerw = chartw - leftp - rightp,
+        chartInnerh = charth - topBottomp * 2,
+        translate = 'translate(' + leftp + ',' + topBottomp +')';
+    
     var chart = d3.select('body')
         .append('svg')
         .attr('width', chartw)
         .attr('height', charth)
         .attr('class', 'chart');
+
+    var chartBg = chart.append('rect')
+        .attr('class','chartBg')
+        .attr('width', chartInnerw)
+        .attr('height', chartInnerh)
+        .attr('transform', translate);
+
+    var yScale = d3.scaleLinear()
+        .range([463,0])
+        .domain([0,100]);
+
+
+    var bars = chart.selectAll('.bars')
+        .data(csvData)
+        .enter()
+        .append('rect')
+        .sort(function(a, b){
+            return a[expressed]-b[expressed];
+        })
+        .attr('class', function(d){
+            return "bars" + d.OBJECTID;
+        })
+        .attr('width', chartw / csvData.length - 1)
+        .attr('x', function(d, i){
+            return i * (chartw / csvData.length);            
+        })
+        .attr('height', function(d){
+            return yScale(parseFloat(d[expressed]));
+        })
+        .attr('y',function(d){
+            return charth - yScale(parseFloat(d[expressed]));
+        })
+        .style('fill', function(d){
+            return choropleth(d, colorScale);
+        });
+    
+    var numbers = chart.selectAll('.numbers')
+        .data(csvData)
+        .enter()
+        .append('text')
+        .sort(function(a,b){
+            return a[expressed]-b[expressed];
+        })
+        .attr('class',function(d){
+            return "numbers" + d.OBJECTID;
+        })
+        .attr('text-anchor','middle')
+        .attr('x', function(d,i){
+            var fraction = chartw / csvData.length;
+            return i * fraction + (fraction - 1) / 2;
+        })
+        .attr('y', function(d){
+            return charth - ySacle(parseFloat(d[expressed])) + 15;
+        })
+        .text(function(d){
+            return d[expressed];
+        });
+
+    var chartTitle = chart.append('text')
+        .attr('x',20)
+        .attr('y',40)
+        .attr('class','chartTitle')
+        .text('Number of Variable' + expressed + 'in each region');
+
+    var yAxis = d3.axisLeft()
+        .scale(yScale)
+        .orient('left');
+    
+    var axis = chart.append('g')
+        .attr('class', 'axis')
+        .attr('transform', translate)
+        .call(yAxis);
+    
+    var chartFrame = chart.append('rect')
+        .attr('class', 'chartFrame')
+        .attr('width', chartInnerw)
+        .attr('height', chartInnerh)
+        .attr('transform', translate)
 };
 
 function makeColorScale(data){
