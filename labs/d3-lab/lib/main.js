6fde97e45ad12f5a6c865d1543dcd4e9821d3a16
Brandon Buchs <bbuchs@wisc.edu>
2019-08-05 13:23:13 -0400

    update main.js and topojson files

--- labs/d3-lab/lib/main.js
+++ labs/d3-lab/lib/main.js
@@ -2,19 +2,14 @@
 window.onLoad = setMap();
 
 function setMap() {
-    d3.queue()
-        .defer(d3.json, 'data/trout.topojson')
-        .defer(d3.csv, 'data/VAStockedTroutStreams.csv')
-        .defer(d3.json, 'data/trout.topojson')
-        .defer(d3.json, 'data/state.topojson')
-        .await(callback);
-        
+    var jsonFiles = ['data/crashes.topojson', 'data/chapelhill.topojson'];
 
-    function callback(topoJson, csvData, trout, state) {
-        console.log('topoJson:', topoJson);
-        console.log('csv:', csvData);
-        console.log('trout.topojson: ', trout);
-        console.log('state.topojson: ', state);
-    };
-    
+    Promise.all(jsonFiles.map(url => d3.json(url))).then(function(values){
+        console.log(values);
+    });
+
+    var csvData = d3.csv('data/crashes.csv');
+    console.log('csvData:', csvData);
 };
+
+ 
